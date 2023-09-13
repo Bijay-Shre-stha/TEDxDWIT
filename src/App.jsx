05bebddebd12bc162838a5c91ceda36a522ec087
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2023-09-13 08:13:09 +0545

    created file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Counter from '../src/Components/Counter'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Counter/>
+      </div>
+  )
+}
+
+export default App
